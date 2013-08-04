956f1e85df1e77b9f1a924379f753a45b28df102
Ben Mather <bwhmather@bwhmather.com>
2013-08-04 13:53:53 +0100

    don't exit if inotifywait fails

--- run-dev-server.sh
+++ run-dev-server.sh
@@ -12,10 +12,11 @@ build;
 
 cd _site
 python -m http.server &
-trap "kill $!; exit 0;" SIGINT SIGTERM
+server_pid=$!
+trap "kill $server_pid; exit 0;" SIGINT SIGTERM
 
 cd ..
-while inotifywait -qe MOVE_SELF -- `find . -name '*'`;
-do
+while true; do
+    inotifywait -qe MOVE_SELF -- `find . -name '*'`;
     build;
 done
